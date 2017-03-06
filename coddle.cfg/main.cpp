dfc8f5dbabfca6b45709a6b3590534883bde78d6
Anton Te <te.anton@gmail.com>
2017-03-05 18:29:28 -0800

    Remove coddle config

--- coddle.cfg/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <coddle/coddle.hpp>
-#include <coddle/config.hpp>
-
-int main(int argc, char **argv)
-{
-  Config config(argc, argv);
-  config.pkgs.insert(std::end(config.pkgs), { "sdl2" , "libavdevice", "libavformat", "libavutil", "libavcodec" });
-  return coddle(&config);
-}
